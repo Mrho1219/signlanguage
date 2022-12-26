507ffeba3f36b189d4c7dcc1a73e10794c63f9ae
Mrho1219 <mrhoho1219@gmail.com>
2022-12-26 17:16:45 +0900

    Create SignLanguageModel.py

---
+++ SignLanguage/SignLanguageModel.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+from matplotlib import pyplot as plt
+from tensorflow import keras
+from scipy import stats
+
+
+class SignLanguage():
+    def __init__(self):
+        self.mp_holistic = mp.solutions.holistic # Holistic model
+        self.mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+        self.model = keras.models.load_model('action2.h5')
+        
+        # Actions that we try to detect
+        self.actions = np.array(['mouse', 'road', 'money', 'x'])
+        
+    def mediapipe_detection(self, image, model):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+        image.flags.writeable = False                  # Image is no longer writeable
+        results = model.process(image)                 # Make prediction
+        image.flags.writeable = True                   # Image is now writeable 
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+        return image, results
+
+    def draw_styled_landmarks(self, image, results):
+        # Draw pose connections
+        self.mp_drawing.draw_landmarks(image, results.pose_landmarks, self.mp_holistic.POSE_CONNECTIONS,
+                                self.mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
+                                self.mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                                ) 
+        # Draw left hand connections
+        self.mp_drawing.draw_landmarks(image, results.left_hand_landmarks, self.mp_holistic.HAND_CONNECTIONS, 
+                                self.mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
+                                self.mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                                ) 
+        # Draw right hand connections  
+        self.mp_drawing.draw_landmarks(image, results.right_hand_landmarks, self.mp_holistic.HAND_CONNECTIONS, 
+                                self.mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
+                                self.mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                                ) 
+
+    def extract_keypoints(self, results):
+        pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+        lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+        rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+        return np.concatenate([pose, lh, rh])
+
+
+    def prob_viz(self, res, actions, input_frame, colors):
+        print(type(res))
+        output_frame = input_frame.copy()
+        for num, prob in enumerate(res):
+            cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
+            cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+            
+        return output_frame
