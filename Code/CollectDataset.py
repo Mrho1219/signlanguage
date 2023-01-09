5c3facaa03dd2d2d80e7990abc060f127591e812
Mrho1219 <mrhoho1219@gmail.com>
2023-01-09 16:12:50 +0900

    Update CollectDataset.py

--- Code/CollectDataset.py
+++ Code/CollectDataset.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
 import os
-from matplotlib import pyplot as plt
 import mediapipe as mp
 
 mp_holistic = mp.solutions.holistic # Holistic model
 mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+
+Target = '길'
 
 def mediapipe_detection(image, model):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
@@ -31,42 +32,7 @@ def draw_styled_landmarks(image, results):
                              mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
                              mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
                              ) 
-
-# cap = cv2.VideoCapture(0)
-# # Set mediapipe model 
-# with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-#     while cap.isOpened():
-
-#         # Read feed
-#         ret, frame = cap.read()
-
-#         # Make detections
-#         image, results = mediapipe_detection(frame, holistic)
-#         print(results)
         
-#         # Draw landmarks
-#         draw_styled_landmarks(image, results)
-
-#         # Show to screen
-#         cv2.imshow('OpenCV Feed', image)
-
-#         # Break gracefully
-#         if cv2.waitKey(10) & 0xFF == ord('q'):
-#             break
-#     cap.release()
-#     cv2.destroyAllWindows()
-    
-# pose = []
-# for res in results.pose_landmarks.landmark:
-#     test = np.array([res.x, res.y, res.z, res.visibility])
-#     pose.append(test)
-    
-# pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
-# face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(1404)
-# lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
-# rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
-
-
 def extract_keypoints(results):
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
@@ -76,11 +42,11 @@ def extract_keypoints(results):
 # result_test = extract_keypoints(results)
 
 # Path for exported data, numpy arrays
-DATA_PATH = os.path.join('MP_Data') 
+DATA_PATH = os.path.join('Data/TrainData') 
 
 
 # Actions that we try to detect
-actions = np.array(['길'])
+actions = np.array([Target])
 
 try:
     os.makedirs(f'{DATA_PATH}/{actions[0]}')
