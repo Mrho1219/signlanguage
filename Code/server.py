6c71b898f6108b8ae1b6a36662b237ca8051b307
Mrho1219 <mrhoho1219@gmail.com>
2023-01-10 11:11:41 +0900

    Update server.py

--- Code/server.py
+++ Code/server.py
@@ -16,16 +16,14 @@ class Server():
     def __init__(self):
         plt.figure(figsize=(18,18))
         self.sl = SignLanguage()
-        self.colors = [(245,117,16), (117,245,16), (16,117,245), (185,90,255)]
         self.sequence = []
         self.sentence = []
         self.predictions = []
         self.threshold = 0.8
         
-    async def sendMessage(self, websocket):
-        data_rcv = await websocket.recv(); # receiving the data from client. 
-        print("발신 데이터 " + data_rcv); 
-        await websocket.send(data_rcv); # send received data    
+    async def sendMessage(self, websocket, data):
+        print("송신 : ", data)
+        await websocket.send(data); # send received data    
         
     async def accept(self, websocket, path): 
         print("client connected")
@@ -59,20 +57,20 @@ class Server():
                     self.predictions.append(np.argmax(res))
                                         
                     #3. add to word list
-                    if self.predictions[-7:].count(self.predictions[-1]) >= 10:
+                    if self.predictions[-7:].count(self.predictions[-1]) >= 7:
                         if res[np.argmax(res)] > self.threshold:
-                            # if len(self.sentence) > 0: 
-                            #     if self.sl.actions[np.argmax(res)] != self.sentence[-1]:
-                            #         self.sentence.append(self.sl.actions[np.argmax(res)])
-                            #         self.sendMessage(websocket)
+                            if len(self.sentence) > 0: 
+                                if self.sl.actions[np.argmax(res)] != self.sentence[-1]:
+                                    self.sentence.append(self.sl.actions[np.argmax(res)])
+                                    await self.sendMessage(websocket, self.sentence[-1])
 
-                            # else:
-                            self.sentence.append(self.sl.actions[np.argmax(res)])
-                            self.sendMessage(websocket)
+                            else:
+                                self.sentence.append(self.sl.actions[np.argmax(res)])
+                                await self.sendMessage(websocket, self.sentence[-1])
                                 
                     
                     # print total five words
-                    if len(self.sentence) > 5: 
+                    if len(self.sentence) > 5:
                         self.sentence = self.sentence[-5:]
 
                 cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
