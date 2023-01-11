6bdced15871b4e436641e7d42ce7bb06ec7e20f7
Mrho1219 <mrhoho1219@gmail.com>
2023-01-11 16:40:20 +0900

    Update RealtimeModel.py

--- Code/RealtimeModel.py
+++ Code/RealtimeModel.py
@@ -4,6 +4,7 @@ import numpy as np
 import mediapipe as mp
 from matplotlib import pyplot as plt
 from tensorflow import keras
+from SignLanguageModel import SignLanguage
 
 
 mp_holistic = mp.solutions.holistic # Holistic model
@@ -12,36 +13,7 @@ mp_drawing = mp.solutions.drawing_utils # Drawing utilities
 model = keras.models.load_model('Model/model.h5')
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
 
-def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
-    image.flags.writeable = False                  # Image is no longer writeable
-    results = model.process(image)                 # Make prediction
-    image.flags.writeable = True                   # Image is now writeable 
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
-    return image, results
-
-def draw_styled_landmarks(image, results):
-    # Draw pose connections
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
-                             mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
-                             ) 
-    # Draw left hand connections
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
-                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
-                             ) 
-    # Draw right hand connections  
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
-                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-                             ) 
-
-def extract_keypoints(results):
-    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
-    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
-    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
-    return np.concatenate([pose, lh, rh])
+sl = SignLanguage()
 
 # Load CSV File
 sign = []
@@ -69,14 +41,14 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         ret, frame = cap.read()
         
         # Make detections
-        image, results = mediapipe_detection(frame, holistic)
+        image, results = sl.mediapipe_detection(frame, holistic)
         print(results)
         
         # Draw landmarks
-        draw_styled_landmarks(image, results)
+        sl.draw_styled_landmarks(image, results)
         
         # 2. Prediction logic
-        keypoints = extract_keypoints(results)
+        keypoints = sl.extract_keypoints(results)
         sequence.append(keypoints)
         sequence = sequence[-15:]
         
