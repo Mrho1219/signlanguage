9817990c1daa0939ceea132f36617a2727526184
Mrho1219 <mrhoho1219@gmail.com>
2023-01-16 11:10:52 +0900

    Update NaverDictionaryParsing.py

--- Code/NaverDictionaryParsing.py
+++ Code/NaverDictionaryParsing.py
@@ -15,48 +15,50 @@ class NaverDictionaryParsing():
     
     def getThumnail(self, word, url):
         
-        print(url)
-        if url == '':
-            encUrl = "https://openapi.naver.com/v1/search/encyc"
-            encQuery = "?query=" + urllib.parse.quote(word)
-            encOption = "&display=1&sort=count"
-            url = encUrl + encQuery + encOption
-            # request url 생성
-
-            request = urllib.request.Request(url)
-            request.add_header("X-Naver-Client-Id",client_id)
-            request.add_header("X-Naver-Client-Secret",client_secret)
-            # request 객체 생성 및 header에 id&secret 추가
-
-            response = urllib.request.urlopen(request)
-            # urlopen 및 response 획득
+        with open('Data\\SignList.csv', 'r') as rf:
+            r = csv.reader(rf)
             
-            rescode = response.getcode()
-            if(rescode==200):
-                json_rt = response.read().decode('utf-8')
-                py_rt = json.loads(json_rt)
-                items = py_rt['items']
+            if url == '':
+                encUrl = "https://openapi.naver.com/v1/search/encyc"
+                encQuery = "?query=" + urllib.parse.quote(word)
+                encOption = "&display=1&sort=count"
+                url = encUrl + encQuery + encOption
+                # request url 생성
+
+                request = urllib.request.Request(url)
+                request.add_header("X-Naver-Client-Id",client_id)
+                request.add_header("X-Naver-Client-Secret",client_secret)
+                # request 객체 생성 및 header에 id&secret 추가
+
+                response = urllib.request.urlopen(request)
+                # urlopen 및 response 획득
                 
-                lines = []
-                with open('Data\\SignList.csv', 'r') as f:
-                    r = csv.reader(f)
+                rescode = response.getcode()
+                if(rescode==200):
+                    json_rt = response.read().decode('utf-8')
+                    py_rt = json.loads(json_rt)
+                    items = py_rt['items']
+                    
+                    lines = []
+                    
                     for line in r:
                         if line[0] == word:
                             line[2] == items[0]['thumbnail']
-                            
-                with open('Data\\SignList.csv','w') as f:
-                    wr = csv.writer(f)
-                    wr.writerows(lines)
-                    
-                return items[0]['thumbnail']
+                        lines.append(line)
+                        
+                    print(lines)
+                                
+                    with open('Data\\SignList.csv','w') as wf:
+                        w = csv.writer(wf)
+                        w.writerows(lines)
+                        
+                    return items[0]['thumbnail']
+                else:
+                    return "Error Code:" + rescode
             else:
-                return "Error Code:" + rescode
-        else:
-            with open('Data\\SignList.csv', 'r') as f:
-                    r = csv.reader(f)
-                    for line in r:
-                        if line[0] == word:
-                            return line[2]
+                for line in r:
+                    if line[0] == word:
+                        return line[2]
   
 # HTTP status code 확인하여 response 데이터 디코딩
 
